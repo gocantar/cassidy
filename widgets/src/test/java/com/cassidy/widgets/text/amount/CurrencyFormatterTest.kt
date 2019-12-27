c3806327f9b0fdd1f21cf242e0b4f09f0c9f2916
Gonzalo Cantarero Perez <gonzalo.cantarero.contractor@bbva.com>
2019-12-27 21:04:33 +0100

    test: add some test cases to CurrencyFormaterTests.kt

--- widgets/src/test/java/com/cassidy/widgets/text/amount/CurrencyFormatterTest.kt
+++ widgets/src/test/java/com/cassidy/widgets/text/amount/CurrencyFormatterTest.kt
@@ -5,11 +5,14 @@ import com.cassidy.widgets.text.amount.models.CurrencyFormat
 import com.gocantar.cassidy.test.base.UnitTest
 import com.gocantar.cassidy.test.extensions.equal
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.math.BigDecimal
 import java.util.stream.Stream
+import kotlin.test.assertNull
 
 /**
  * @author Gonzalo Cantarero Pérez, Dic 2019
@@ -21,7 +24,7 @@ class CurrencyFormatterTest : UnitTest {
     @DisplayName("Given positive amount")
     @ParameterizedTest(name = "Should be format to {1}")
     @MethodSource("positiveParameterizedArguments")
-    fun shouldFormatPositiveAmount(format: CurrencyFormat, amountFormatted: String) {
+    fun shouldFormatPositiveAmount(format: CurrencyFormat?, amountFormatted: String) {
         val formatter = CurrencyFormatter()
         val amount = BigDecimal.valueOf(999999.99)
         val result = formatter.format(amount, format)
@@ -32,12 +35,25 @@ class CurrencyFormatterTest : UnitTest {
     @DisplayName("Given negative amount")
     @ParameterizedTest(name = "Should be format to {1}")
     @MethodSource("negativeParameterizedArguments")
-    fun shouldFormatNegativeAmount(format: CurrencyFormat, amountFormatted: String) {
+    fun shouldFormatNegativeAmount(format: CurrencyFormat?, amountFormatted: String) {
         val formatter = CurrencyFormatter()
         val amount = BigDecimal.valueOf(-999999.99)
         val result = formatter.format(amount, format)
         result equal amountFormatted
     }
+
+    @Nested
+    @DisplayName("Given null amount")
+    inner class NullAmountTest {
+        @Test
+        @DisplayName("Should return null")
+        fun whenAmountIsNull_shouldReturnNull() {
+            val formatter = CurrencyFormatter()
+            val result = formatter.format(null, format01)
+            assertNull(result)
+        }
+    }
+
 
     companion object {
         @JvmStatic
@@ -46,7 +62,8 @@ class CurrencyFormatterTest : UnitTest {
                 Arguments.of(format01, "999.999,99€"),
                 Arguments.of(format02, "999.999,99€"),
                 Arguments.of(format03, "€999,999.99"),
-                Arguments.of(format04, "€999,999.99")
+                Arguments.of(format04, "€999,999.99"),
+                Arguments.of(null, "999.999,99")
             )
         }
 
@@ -56,7 +73,8 @@ class CurrencyFormatterTest : UnitTest {
                 Arguments.of(format01, "-999.999,99€"),
                 Arguments.of(format02, "-999.999,99€"),
                 Arguments.of(format03, "-€999,999.99"),
-                Arguments.of(format04, "€-999,999.99")
+                Arguments.of(format04, "€-999,999.99"),
+                Arguments.of(null, "-999.999,99")
             )
         }
 
